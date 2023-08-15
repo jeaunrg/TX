d184e784c6d73ff0a0a6f2ad514bd20eab59e8c4
jeaunrg <jeanunrug@gmail.com>
2023-08-15 16:42:11 +0200

    first commit

---
+++ config.py
@@ -0,0 +1,12 @@
+class HealthConfig:
+    plafond_secu_social = 3666
+    alan_green = 56
+    compl_deces_ta = 0.0044
+    compl_deces_tb = 0.0072
+    compl_cotisation_pat = 0.495
+    base_compl = 0.9825
+    csg_deductible = 0.068
+    retraite_secu_sociale_plaf = 0.069
+    retraite_secu_sociale = 0.004
+    retraite_compl1 = 0.0415
+    retraite_compl2 = 0.0986
