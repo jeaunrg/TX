f3d75c2668704bbc8efb54b77509e33a5ad950f5
jean <57048964+jeaunrg@users.noreply.github.com>
2023-09-15 13:59:24 +0000

    rm os

--- tx_site/tx_site/settings.py
+++ tx_site/tx_site/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
-import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -129,13 +128,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    os.path.join(BASE_DIR, "media"),
+    BASE_DIR / "static",
+    BASE_DIR / "media",
 ]
 STATIC_URL = "/static/"
 MEDIA_URL = "/media/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static_cdn")
-MEDIA_ROOT = os.path.join(BASE_DIR, "media_cdn")
+STATIC_ROOT = BASE_DIR / "static_cdn"
+MEDIA_ROOT = BASE_DIR / "media_cdn"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
